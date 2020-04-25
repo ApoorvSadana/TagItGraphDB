30ec2acebdf91f3475357b9643bed7497fd72e0f
ApoorvSadana <42446506+ApoorvSadana@users.noreply.github.com>
2020-04-26 01:27:25 +0530

    server changes

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,9 @@
-const neo4j = require('neo4j-driver');
+const neo4j = require("neo4j-driver");
 
 // Connect to neo4j database
-const driver = neo4j.driver('bolt://localhost:7687', neo4j.auth.basic('neo4j', 'tag$100$'))
+const driver = neo4j.driver(
+  "bolt://3.92.183.49:7687",
+  neo4j.auth.basic("neo4j", "tag$100$")
+);
 
 module.exports = driver;
