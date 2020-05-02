0fb8bbc8abd901b20185fc6f18660e0cd0b3a52f
ApoorvSadana <42446506+ApoorvSadana@users.noreply.github.com>
2020-05-02 18:31:24 +0530

    changed server ip

--- server/models/index.js
+++ server/models/index.js
@@ -2,7 +2,7 @@ const neo4j = require("neo4j-driver");
 
 // Connect to neo4j database
 const driver = neo4j.driver(
-  "bolt://13.235.51.29:7687",
+  "bolt://13.234.132.1:7687",
   neo4j.auth.basic("neo4j", "tag$100$")
 );
 
